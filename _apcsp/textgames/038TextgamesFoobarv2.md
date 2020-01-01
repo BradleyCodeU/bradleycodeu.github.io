0fa04e7f5a64fe21dbc21f498c130d32176eca27
ohiofi <teacher@HCodeUMac-T1513.local>
2020-01-01 02:13:52 -0500

    textgames

--- _apcsp/textgames/038TextgamesFoobarv2.md
+++ _apcsp/textgames/038TextgamesFoobarv2.md
@@ -3,18 +3,22 @@ layout: project
 category: textgames
 title: Text Games Foobar v2
 ---
-Visit [http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Game%20Title](http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Game%20Title) to get an ASCII art version of your title.
 
 [Copy/Paste this map class](https://gist.githubusercontent.com/ohiofi/a1fc5080137a832ea42bc1fa7adc8318/raw/f647a82336357a3a4b09c1a2a834c56e61b3bb64/map.py) into a new file, map.py
 
 You will be graded on the following requirements:
 
-1. Have 10 rooms for each person in your group (group of 3 = 30 rooms, group of 4 = 40 rooms). Each of these rooms must have a unique description. NOT just "you're in a room". Have 2 or more items for each person in your group.
-2. Have 1 *special room* for each person in your group. A special room could be: a battle with an enemy, a high-low number guessing game (to open a combination lock, for example), a fill-in-the-blank quiz game (to figure out a computer password, for example), or a locked door that is unlocked if the player has the key in their inventory/backpack and types "use key"
-3. Must use the Map class to draw a map for your game (hide or show the items is up to you).
-4. Print the room description, any item in the room, any items in the player's inventory/backpack, and the possible commands that the player can type.
-5. Must be able to change locations and give a message like "You can't go there" when necessary.
-6. Must be able to collect an item (using "take"), place the item in the player's inventory/backpack, and remove the item from the room.
-7. Must have a break in your while loop so that game ends if the user types "quit", loses the game, or wins the game.
+1. Figure out a title for you game and visit [this site](http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Game%20Title) to get an ASCII art version of your title.
+1. More rooms (group of 3 = 21 total rooms, group of 4 = 28 total rooms). Each of these rooms must have a unique description. NOT just "you're in a room". Have 2 or more items for each person in your group.
+1. Have 1 *special room* for each person in your group. A special room could be:
+  - a battle with an enemy
+  - a high-low number guessing game (to open a combination lock, for example)
+  - a fill-in-the-blank quiz game (to figure out a computer password, for example)
+  - a locked door that is unlocked if the player has the key in their inventory/backpack and types "use key"
+1. Must use the Map class to draw a map for your game (hide or show the items is up to you).
+1. Print the room description, any item in the room, any items in the player's inventory/backpack, and the possible commands that the player can type.
+1. Must be able to change locations and give a message like "You can't go there" when necessary.
+1. Must be able to collect an item (using "take"), place the item in the player's inventory/backpack, and remove the item from the room.
+1. Must have a break in your while loop so that game ends if the user types "quit", loses the game, or wins the game.
 
-*** Put your finished game on Trinket.io with the map.py class. Turn in the URL for your finished game. ***
+``` Put your finished game on Trinket.io along with the map.py class. Turn in the URL for your finished game. ```
--- _apcsp/textgames/castlesAndCreaturesTemplate.py
+++ _apcsp/textgames/castlesAndCreaturesTemplate.py
@@ -1,12 +1,14 @@
-# Castles and Creatures by Justin Riley
-# from random import everything
-# from time import everything
+# Castles & Creatures by Justin Riley
+# import random
+# import time
 
 
-# create a function called askPlayer that will return the number that the player selects
+# create a function called askPlayer that will prompt, collect input,
+# & return the number that the player selects
 # No arguments needed... so it'll look like this: askPlayer()
-# Ask the player if they want to try to steal or use one of their weapons (for example, 1=Steal 2=Use fire sword 3=Use ice dagger 4=Use lightning staff)
-# Validate Input. If the user types something wrong, say "Not a number" and repeat the question.
+# Ask the player if they want to try to steal or use one of their weapons
+# (for example, 1=Steal 2=Use fire sword 3=Use ice dagger 4=Use lightning staff)
+# Validate Input. If input is wrong, say "Not a number" & repeat the question.
 # Return the number that they choose.
 def askPlayer():
     while True:
@@ -21,14 +23,10 @@ def askPlayer():
 
 
 # create a function called tryToSteal that will return True or False
-# There is one argument, enemyName, so it will look like this: tryToSteal(enemyName)
-# If the enemyName equals your final boss (for example, Mutant Spider), then say you cannot steal from enemyName and return False.
-# Stealing always has a 20% chance of succeeding.
-# 20% of the time, print a message telling the user that they were able to steal and return True
-# Otherwise, print a message telling the user they were NOT able to steal and return False (80% of the time)
-def tryToSteal(enemyName):
-    if
-
+# Stealing always has a 10% chance of succeeding.
+# 10% of the time, tell the user that they were able to steal & return True
+# Otherwise (90%), tell the user they were NOT able to steal & return False
+def tryToSteal():
     if
         # sleep for .5 seconds
 
@@ -42,15 +40,17 @@ def tryToSteal(enemyName):
 
 
 # create a function called enemyLoseHealth that will return the newEnemyHealth
-# there are 4 arguments: playerChoice, enemyHealth, enemyName, enemyWeakness
-# like this, enemyLoseHealth(playerChoice, enemyHealth, enemyName, enemyWeakness)
-# if the playerChoice equals the enemyWeakness, then generate a large random int and store it in a variable called damage
-# otherwise, generate a smaller random int and store it in a variable called damage
-# if damage is greater than some amount, print something like "IT'S SUPER EFFECTIVE!!!!!"
-# print -damage to enemyName
-# create a variable called newEnemyHealth and set it to enemyHealth minus damage
+# there are 2 arguments: playerChoice, enemy
+# if the playerChoice equals enemy["weakness"],
+#   then generate a large random int & store it in a variable called damage
+# otherwise,
+#   generate a smaller random int & store it in a variable called damage
+# if damage is greater than some amount,
+#   then print "IT'S SUPER EFFECTIVE!!!!!"
+# print -damage to enemy["name"]
+# create a variable called newEnemyHealth & set it to enemy["health"] minus damage
 # return newEnemyHealth
-def enemyLoseHealth(playerChoice, enemyHealth, enemyName, enemyWeakness):
+def enemyLoseHealth(playerChoice, enemy):
     if
 
     else:
@@ -65,18 +65,19 @@ def enemyLoseHealth(playerChoice, enemyHealth, enemyName, enemyWeakness):
 
 
 # create a function called playerLoseHealth that will return the newPlayerHealth
-# there are 4 arguments: playerHealth, enemyName, enemyLevel, enemyAttacks
-# Like this, playerLoseHealth(playerHealth, enemyName, enemyLevel, enemyAttacks)
-# Create a variable called damage and set it to a random int between 0 and the enemyLevel
-# Shuffle the enemyAttacks array list
-# Create a variable called randomAttack and set it to enemyAttacks[0]
-# Print a message that tells you how much the enemy hurt you (for example, enemyName used randomAttack! -damage HP)
-# Create a variable called newPlayerHealth that is equal to playerHealth minus damage
+# there are 2 arguments: playerHealth, enemy
+# Create a variable called damage
+# Set damage to a random int between 0 & the enemy["level"]
+# Create a variable called randomAttack
+# Set randomAttack to a random choice from the the enemy["attacks"] array list
+# Print a message that tells you how much the enemy hurt you
+# (for example, enemy["level"] used randomAttack! -damage HP)
+# Create a newPlayerHealth variable that is equal to playerHealth minus damage
 # Print a message that says You have newPlayerHealth HP
 # Return the newPlayerHealth
-def
+def playerLoseHealth(playerHealth, enemy):
 
-    shuffle(
+    random.choice(enemy["attacks"])
 
     # sleep for .5 seconds
 
@@ -89,11 +90,10 @@ def
 
 
 # create a function called gameLevel that will return playerHealth
-# this function needs lots of arguments: castleDescription,enemyName,enemyLevel,enemyHealth,enemyAttacks,enemyWeakness,playerHealth
-# Like this, gameLevel(castleDescription,enemyName,enemyLevel,enemyHealth,enemyAttacks,enemyWeakness,playerHealth)
+# this function needs three arguments: castleDescription,enemy,playerHealth
 #
-def
-    # create a variable called stealSuccess and set it to False
+def gameLevel(castleDescription,enemy,playerHealth):
+    # create a variable called stealSuccess & set it to False
 
     # print a dividing line like this ==============================================================
 
@@ -107,24 +107,24 @@ def
 
         # print a small dividing line like this ~ ~ ~
 
-        # create a variable called playerChoice and set it to askPlayer()
+        # create a variable called playerChoice & set it to askPlayer()
 
 
         if playerChoice == 1:
-            # set stealSuccess to tryToSteal(enemyName)
-
+            # set stealSuccess to tryToSteal(enemy["name"])
+            stealSuccess =
         else:
-            # A random amount of enemy's health is lost. set enemyHealth to enemyLoseHealth(playerChoice, enemyHealth, enemyName, enemyWeakness)
+            # A random amount of enemy's health is lost. set enemy["health"] to enemyLoseHealth(playerChoice, enemy)
 
 
         # If enemy is still alive
         if
-            # A random amount of player's health lost. set playerHealth to playerLoseHealth(playerHealth, enemyName, enemyLevel, enemyAttacks)
+            # A random amount of player's health lost. set playerHealth to playerLoseHealth(playerHealth, enemy)
 
         else:
             # sleep for .5 seconds
 
-            # print You have defeated enemyName!
+            # print You have defeated enemy["name"]!
 
             # break out of this loop so that you can go to the next level
             break
@@ -136,7 +136,7 @@ def
             # print Oof.  You have DIED!  R.I.P.  Game Over.
         # if you were able to steal
         if
-            # print You steal the treasure and manage to escape past enemyName!
+            # print You steal the treasure & manage to escape past enemy["name"]!
 
             # break out of this loop so that you can go to the next level
             break
@@ -168,37 +168,40 @@ sleep(1)
 # Sleep 1 sec
 sleep(1)
 # Setup castle 1
-# Write a sentence to describe the castle
+# Write a sentence to describe the first castle
 castleDescription =
+# Create an enemy "dictionary" object
+enemy = {}
 # Give the enemy a name
-enemyName =
-# Set enemyLevel to an integer number, maybe 10?
-
-# Set enemyHealth to an integer number, maybe 10?
-
+enemy["name"] =
+# Set the enemy's level to an integer number, maybe 10?
+enemy["level"] =
+# Set the enemy's health to an integer number, maybe 10?
+enemy["health"] =
 # Here are some possible attacks that the enemy could use. You can delete these, create your own, add more, etc.
-enemyAttacks = ["Sneak Attack", "Roundhouse Kick", "Punchy Punch", "Hair Whip", "Jump Scare", "Evil Slap"]
-# Set the enemyWeakness to a random integer number between 2 and 4
-enemyWeakness =
+enemy["attacks"] = ["Sneak Attack", "Roundhouse Kick", "Punchy Punch", "Hair Whip", "Jump Scare", "Evil Slap"]
+# Set the enemyWeakness to a random integer number between 2 & 4
+enemy["weakness"] =
+
 # Create Level 1 by calling gameLevel()
-playerHealth = gameLevel(castleDescription,enemyName,enemyLevel,enemyHealth,enemyAttacks,enemyWeakness,playerHealth)
-# If you wanna add some story between castle 1 and castle 2, you'd do that here with some sleep 1 sec and print statements
+playerHealth = gameLevel(castleDescription,enemy,playerHealth)
+# If you wanna add some story between castle 1 & castle 2, you'd do that here with some sleep 1 sec & print statements
 # Setup castle 2
 # Set castleDescription to a sentence that describes the castle
-# Set enemyName to a name
-# Set enemyLevel to an integer number
-# Set enemyHealth to an integer number
-# Set enemyAttacks to a list of some possible attacks that the enemy could use.
-# Set the enemyWeakness to a random integer number between 2 and 4
+# Set enemy name to a new name
+# Set enemy level to a new integer number
+# Set enemy health to a new integer number
+# Set enemy attacks to a new list of some possible attacks that the enemy could use.
+# Set the enemy weakness to a random integer number between 2 & 4
 # Create Level 2 by calling gameLevel()
-playerHealth = gameLevel(castleDescription,enemyName,enemyLevel,enemyHealth,enemyAttacks,enemyWeakness,playerHealth)
-# If you wanna add some story between castle 2 and castle 3, you'd do that here with some sleep 1 sec and print statements
+playerHealth = gameLevel(castleDescription,enemy,playerHealth)
+# If you wanna add some story between castle 2 & castle 3, you'd do that here with some sleep 1 sec & print statements
 # Setup castle 3
 # Set castleDescription to a sentence that describes the castle
-# Set enemyName to a name
-# Set enemyLevel to an integer number
-# Set enemyHealth to an integer number
-# Set enemyAttacks to a list of some possible attacks that the enemy could use.
-# Set the enemyWeakness to a random integer number between 2 and 4
+# Set enemy name to a new name
+# Set enemy level to a new integer number
+# Set enemy health to a new integer number
+# Set enemy attacks to a new list of some possible attacks that the enemy could use.
+# Set the enemy weakness to a random integer number between 2 & 4
 # Create Level 3 by calling gameLevel()
 playerHealth = gameLevel(castleDescription,enemyName,enemyLevel,enemyHealth,enemyAttacks,enemyWeakness,playerHealth)
