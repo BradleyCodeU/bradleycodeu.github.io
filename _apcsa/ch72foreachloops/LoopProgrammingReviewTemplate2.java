df7d286affe30f456a9ab42505ac121b4a51c03b
Justin Riley <20hz20khz@gmail.com>
2020-01-07 09:18:52 -0500

    Update LoopProgrammingReviewTemplate2.java

--- _apcsa/ch72foreachloops/LoopProgrammingReviewTemplate2.java
+++ _apcsa/ch72foreachloops/LoopProgrammingReviewTemplate2.java
@@ -4,199 +4,172 @@ public class LoopProgrammingReview
 {
 	public static void main(String[] args)
 	{
-		int progNum = 1;
+		int progNum = 0;
+		System.out.println("*** Program " + progNum + " ***");
+		progNum++;
+		programZero();
+		System.out.println("*** Program " + progNum + " ***");
+		progNum++;
+		programOne();
+		System.out.println("*** Program " + progNum + " ***");
+		progNum++;
+		programTwo();
+		System.out.println("*** Program " + progNum + " ***");
+		progNum++;
+		programThree();
+		System.out.println("*** Program " + progNum + " ***");
+		progNum++;
+		programFour();
+		System.out.println("*** Program " + progNum + " ***");
+		progNum++;
+		programFive();
+	}
+
+
+
+	private void programZero(){
+		String answer;
+		do
 		{
-			System.out.println("*** Program " + progNum + " ***");
-			/*  add or remove // at start of line
-
-
-			String answer;
-
-			do
+			System.out.print("Enter a string that does not contain the letter s: ");
+			answer = in.nextLine();
+			if ()  //ADD
 			{
-				System.out.print("Enter a string that does not contain the letter s: ");
-				answer = in.readString();
-
-				if ()  //ADD
-				{
-					System.out.println("Error - string should not contain the letter s\n");
-				}
-			}while (); //ADD
-
-			System.out.println("Yay - a string with no letter s\n");
-			*/
-			progNum++;
-		}
+				System.out.println("Error - string should not contain the letter s\n");
+			}
+		} while (); //ADD
+		System.out.println("Yay - a string with no letter s\n");
+	}
 
 
 
+	private void programOne(){
+		int num;
+		do
 		{
-			System.out.println("*** Program " + progNum + " ***");
-			/*
+			System.out.print("Enter an int in the range [3,8]: "); //NOTE: When checking your program, be sure to check the boundary/edge cases - particulary the value 3 and 8
+			num = in.nextInt();
 
-
-			int num;
-
-			do
+			if ()	//ADD
 			{
-				System.out.print("Enter an int in the range [3,8]: "); //NOTE: When checking your program, be sure to check the boundary/edge cases - particulary the value 3 and 8
-				num = in.readInt();
-
-				if ()	//ADD
-				{
-					System.out.println("Error - number is less than 3\n");
-				}
-				else if ()	//ADD
-				{
-					System.out.println("Error - number is greater than 8\n");
-				}
-			}while (	);//ADD
-
-			System.out.println("Yay - " + num + " is in the range [3,8]\n");
-			*/
-			progNum++;
-		}
-
-
-		{
-			System.out.println("*** Program " + progNum + " ***");
-			/*
-
-			int num;				//current number entered by user
-			int sum = 0;			//sum of the negative numbers entered
-			int numNegatives = 0; 	//number of negative numbers entered
-
-
-			System.out.println("Please enter four negative numbers below.");
-			System.out.println("The sum of the negative numbers will be calculated and displayed.");
-
-			do
+				System.out.println("Error - number is less than 3\n");
+			}
+			else if ()	//ADD
 			{
-				System.out.print("Enter int #" + (numNegatives + 1)+ ": ");
-				num = in.readInt();
+				System.out.println("Error - number is greater than 8\n");
+			}
+		} while ();//ADD
+		System.out.println("Yay - " + num + " is in the range [3,8]\n");
+	}
 
-				if ()	//ADD
-				{
-					sum = sum + num;
-					numNegatives++;
-				}
-				else
-				{
-					System.out.println("Error - number is not negative\n");
-				}
 
-			}while ();	//ADD
 
-			System.out.println("The sum of the negative number is: " + sum + "\n");
-			*/
-			progNum++;
-		}
-
+	private void programTwo(){
+		int num;				//last number entered by user
+		int sum = 0;			//sum of the negative numbers entered
+		int numNegatives = 0; 	//number of negative numbers entered
+		System.out.println("Please enter four negative numbers below.");
+		System.out.println("The sum of the negative numbers will be calculated and displayed.");
+		do
 		{
-			System.out.println("*** Program " + progNum + " ***");
-			/*
-
-			String answer;
-			String lastCharacter;
-
-			do
+			System.out.print("Enter int #" + (numNegatives + 1)+ ": ");
+			num = in.nextInt();
+			if ()	//ADD
 			{
-				System.out.print("Enter a string that has length of at least 5 and the last letter is an s: ");
-				answer = in.readString();
+				sum = sum + num;
+				numNegatives++;
+			}
+			else
+			{
+				System.out.println("Error - number is not negative\n");
+			}
+		} while ();	//ADD
 
-				lastCharacter = ; //ADD code to find the last character (using substring) and store it in variable lastCharacter
+		System.out.println("The sum of the negative number is: " + sum + "\n");
+	}
 
-				if ()	//ADD
-				{
-					System.out.println("String is too short and the last letter is not an s\n");
-				}
-				else if ()	//ADD
-				{
-					System.out.println("String is too short\n");
-				}
-				else if()	//ADD
-				{
-					System.out.println("last letter is not an s\n");
-				}
 
-			}while ();		//ADD
 
-			System.out.println("Yay - the length is " + answer.length() + " and last letter is an s\n");
-			*/
-			progNum++;
-		}
-
+	private void programThree(){
+		String answer;
+		String lastCharacter;
+		do
 		{
-			System.out.println("*** Program " + progNum + " ***");
-			/*Fill in the code to display the fibonacci sequence
-			The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, etc...
-			The next number is found by adding up the two numbers before it.
-			The 2 is found by adding the two numbers before it (1+1)
-			The 3 is found by adding the two numbers before it (1+2),
-			And the 5 is (2+3) and so on!
-			/*
+			System.out.print("Enter a string that has length of at least 5 and the last letter is an s: ");
+			answer = in.nextLine();
+			lastCharacter = ; //ADD code to find the last character (using substring) and store it in variable lastCharacter
+			if ()	//ADD
+			{
+				System.out.println("String is too short and the last letter is not an s\n");
+			}
+			else if ()	//ADD
+			{
+				System.out.println("String is too short\n");
+			}
+			else if()	//ADD
+			{
+				System.out.println("last letter is not an s\n");
+			}
+		} while ();		//ADD
+		System.out.println("Yay - the length is " + answer.length() + " and last letter is an s\n");
+	}
 
-			ArrayList< > myArray
-			myArray.add(0)
-			myArray.add(1)
-			Scanner in
-			int stoppingPoint
+
+
+	private void programFour(){
+		/*Fill in the code to display the fibonacci sequence
+		The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, etc...
+		The next number is found by adding up the two numbers before it.
+		The 2 is found by adding the two numbers before it (1+1)
+		The 3 is found by adding the two numbers before it (1+2),
+		And the 5 is (2+3) and so on!
+		*/
+		ArrayList< /*ADD*/ > myArray = //ADD
+		myArray.add(0)
+		myArray.add(1)
+		Scanner in
+		int stoppingPoint
+		// prompt for an integer to use as a stopping point
+		// while scanner does not have next int
+		{
+			// print "Not an integer number"
+			in.nextLine(); // throw away everything currently in the scanner
 			// prompt for an integer to use as a stopping point
-			// while scanner does not have next int
+		}
+		while(last value of myArray is less than stoppingPoint)
+		{
+			add together the last two values of myArray add append the sum to the end of the list
+		}
+		print myArray
+	}
+
+
+
+	private void programFive(){
+		// Ask the user for several numbers or Q to quit. Then calculate the average of their numbers.
+		count = 0
+		sum = 0
+		while true // repeat until the use types Q then break
+		{
+			// prompt for an integer or Q to quit.
+			if ??? // if the scanner has next int
+			{
+				// get the next int and add it to the sum
+				// increment the count
+			}
+			else if ??// the scanner's next line is Q
+			{
+				exit the loop
+			}
+			else
 			{
 				// print "Not an integer number"
 				in.nextLine(); // throw away everything currently in the scanner
-				// prompt for an integer to use as a stopping point
 			}
-
-
-			while(last value of myArray is less than stoppingPoint)
-			{
-				add together the last two values of myArray add append the sum to the end of the list
-			}
-			print myArray
-			*/
-			progNum++;
 		}
-
+		if count > 0
 		{
-			System.out.println("*** Program " + progNum + " ***");
-			// Ask the user for several numbers or Q to quit. Then calculate the average of their numbers.
-
-			/*
-			count = 0
-			sum = 0
-
-			while true // repeat until the use types Q then break
-			{
-				// prompt for an integer or Q to quit.
-				if ??? // if the scanner has next int
-				{
-					// get the next int and add it to the sum
-					// increment the count
-				}
-				else if the scanner's next line is Q
-				{
-					exit the loop
-				}
-				else
-				{
-					// print "Not an integer number"
-					in.nextLine(); // throw away everything currently in the scanner
-				}
-			}
-			if count > 0
-			{
-				// calculate and print average
-			}
-
-
-			*/
-
-
+			// calculate and print average
 		}
-
-
 	}
-
 }
