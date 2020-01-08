d771182c691bb7687a04c5df240c24253fb856ba
Justin Riley <20hz20khz@gmail.com>
2020-01-08 13:38:42 -0500

    IntroToAppDevForXcode11

---
+++ IntroToAppDevForXcode11/02Naming.playground/Pages/Good Names.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: ## Choosing Good Names
+//: Choosing good names is a very important part of programming. Programmers want to choose good names because even though it’s important to know how to write code, it’s even more important to be able to read and understand code.
+//:
+//: So far, you’ve used names like `numberOfDogs` and `numberOfCats`. These are good names because they clearly describe what they represent.
+//:
+//: Imagine if the names had been `nd` and `nc` instead (for **n**umber of **d**ogs and **n**umber of **c**ats).\
+//: Or even less descriptive names, like `n1` and `n2`.
+//:
+//: Swift will accept names like this without complaint. But imagine coming back to read that code after vacation. Would you remember what `nd` or `n1` meant? And if you gave the playground to other programmers, how would they figure out what those names were supposed to mean?
+//:
+//:
+//: Next, learn the official names for the work you’ve been doing.
+//:
+//:[Previous](@previous)  |  page 7 of 14  |  [Next: Programmer Talk](@next)
