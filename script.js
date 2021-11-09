ad0b1d100828424cacc12ec6d80d755f428a448f
Justin Riley <20hz20khz@gmail.com>
2021-11-09 11:18:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function turkeyDay() {
   let h4Array = document.getElementsByTagName("h4");
   let anchorArray = document.getElementsByTagName("a");
   for (let i = 0; i < anchorArray.length; i++) {
-    if (!anchorArray[i].classList.contains("nav-link") && Math.random() + Math.random() > 15 / today) {
+    if (!anchorArray[i].classList.contains("nav-link") && !anchorArray[i].classList.contains("navbar-brand") && Math.random() + Math.random() > 15 / today) {
       anchorArray[i].innerHTML = choice(emojis) + " " + anchorArray[i].innerHTML + " " + choice(emojis);
     }
   }
