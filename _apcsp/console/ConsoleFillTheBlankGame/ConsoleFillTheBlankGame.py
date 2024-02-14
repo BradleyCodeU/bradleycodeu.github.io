85d3d8ece27b72f3d6ca52e426e76827fdf10dca
Justin Riley <20hz20khz@gmail.com>
2024-02-14 14:31:29 -0500

    Create ConsoleFillTheBlankGame.py

---
+++ _apcsp/console/ConsoleFillTheBlankGame/ConsoleFillTheBlankGame.py
@@ -0,0 +1,24 @@
+
+# Create an empty array called correctAnswers and an empty array called wrongAnswers
+
+# Define a function called askQuestion. It requires two arguments: questionText, answerText. Inside this function will be:
+  # a while True loop
+    # ask the questionText
+    # collect user input
+    # convert the user input to lowercase letters
+    # if user input does NOT equal the answerText (NOTICE: THIS PLACES THE CONDITION AT THE END OF THE LOOP)
+      # say incorrect try again
+    # else:
+      # say correct
+      # break the loop
+
+# Display instructions for the player like in the Sample Output
+
+# Create at least 6 (or 10 if working with partner) "Fill in the Blank" style questions.
+
+# The game will only move on to another question when you get the question right.
+
+# Print "Your wrong answers were: " and print the array of user's wrongAnswers.
+
+# Finally, print "Your correct answers were: " and print the array of user's correctAnswers.
+
