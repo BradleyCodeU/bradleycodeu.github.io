ecb84cad30e4bfc97f428051d26449536107276a
Justin Riley <20hz20khz@gmail.com>
2024-02-14 14:31:47 -0500

    Create ConsoleRandIntArray.py

---
+++ _apcsp/console/ConsoleRandIntArray/ConsoleRandIntArray.py
@@ -0,0 +1,5 @@
+# You will complete this project in Replit Teams
+
+# You must FIRST go to the BradleyCodeU website to copy/paste your starter code for the project: https://bradleycodeu.github.io/apcsp/console/103ConsoleRandomIntArray/
+
+# Detailed directions are given as comments in the starter code
