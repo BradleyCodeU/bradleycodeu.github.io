15ec2ceadb1c85003855667a1c436fe2e48d5619
Justin Riley <20hz20khz@gmail.com>
2024-02-14 10:10:53 -0500

    Update Tests_PyMagicTrick.py

--- _apcsp/py/PyMagicTrick/Tests_PyMagicTrick.py
+++ _apcsp/py/PyMagicTrick/Tests_PyMagicTrick.py
@@ -1,8 +1,7 @@
-import unittest
 from PyMagicTrick import *
+import unittest
 
-
-class Tests_PyMagicTrick(unittest.TestCase):
+class AutomaticTester(unittest.TestCase):
 
     def test_double(self):
         # Test the double function
@@ -29,7 +28,6 @@ class Tests_PyMagicTrick(unittest.TestCase):
 
 
 class MyTestResult(unittest.TextTestResult):
-
     def __init__(self, stream, descriptions, verbosity):
         super().__init__(stream, descriptions, verbosity)
         self.stream = stream
@@ -43,22 +41,13 @@ class MyTestResult(unittest.TextTestResult):
             self.stream.write("✅  ")
         else:
             super().addSuccess(test)
-
         self.stream.writeln(test._testMethodName)
 
     def addFailure(self, test, err):
         super().addFailure(test, err)
         self.failure_count += 1
-        if self.verbosity > 0:
-            self.stream.write("❌  ")
-        else:
-            self.stream.writeln(f"FAIL: {test._testMethodName}")
-        self.stream.writeln(f"{test._testMethodName}")
-        # self.stream.writeln("Stack Trace:")
-#         self.stream.writeln("Traceback (most recent call last):")
-#         self.stream.writeln("  File \"/home/runner/1212-Weight-On-Planets-KevinAcosta16/_test_runnertest_suite.py\", line 17, in test_validNumber")
-#         self.stream.writeln(f"    {err}")
-
+        self.stream.write("❌")
+        self.stream.writeln(f" {test._testMethodName}")
 
     def startTestRun(self):
         super().startTestRun()
@@ -67,10 +56,24 @@ class MyTestResult(unittest.TextTestResult):
 
     def stopTestRun(self):
         if self.failure_count > 0:
-            self.stream.writeln(f"\n❌  {self.success_count}/{self.success_count + self.failure_count} passed, see errors below")
+            self.stream.writeln(
+                f"❌  {self.success_count}/{self.success_count + self.failure_count} passed, see errors below"
+            )
         else:
-            self.stream.writeln(f"\n✅  {self.success_count}/{self.success_count + self.failure_count} passed!")
+            self.stream.writeln(
+                f"✅  {self.success_count}/{self.success_count + self.failure_count} passed!"
+            )
 
 
-if __name__ == '__main__':
-    unittest.main(testRunner=unittest.TextTestRunner(resultclass=MyTestResult, verbosity=1), exit=False)
+if __name__ == "__main__":
+    result = unittest.TextTestRunner(resultclass=MyTestResult, verbosity=1).run(
+        unittest.TestLoader().loadTestsFromTestCase(AutomaticTester)
+    )
+    if result.failures:
+        failed_test_names = [test._testMethodName for test, _ in result.failures]
+        failed_test_names = [name for name in failed_test_names]
+        for each in failed_test_names:
+            print(f"❌  {each}")
+        print()
+    else:
+        print("✅  All tests passed!\n")
