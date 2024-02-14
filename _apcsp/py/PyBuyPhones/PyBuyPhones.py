ecfa6897d5a182cd36b5b144eb25ae33a89997f2
Justin Riley <20hz20khz@gmail.com>
2024-02-13 21:13:14 -0500

    Create PyBuyPhones.py

---
+++ _apcsp/py/PyBuyPhones/PyBuyPhones.py
@@ -0,0 +1,26 @@
+# replace ALL SIX blanks with the correct code
+# blanks look like this: ???
+
+# define a function called buyPhones
+# requires two argument: myDollars, phonePrice
+def buyPhones( ??? , ??? ):
+    #convert myDollars to a float
+    ???
+    #convert phonePrice to a float
+    ???
+    #create a new variable amountOfPhones
+    #myDollars divided by phonePrice equals amountOfPhones
+    ???
+    #convert amountOfPhones to an int
+    ???
+    return amountOfPhones
+
+# do not edit the following code
+amountOfPhones = buyPhones('399','399')
+print("\nExpected :'399','399' → 1 \nYour code:'399','399' → " + str(amountOfPhones))
+amountOfPhones = buyPhones(398.99,399)
+print("\nExpected : 398.99,399 → 0 \nYour code: 398.99,399 → " + str(amountOfPhones))
+amountOfPhones = buyPhones(600,199.99)
+print("\nExpected : 600,199.99 → 3 \nYour code: 600,199.99 → " + str(amountOfPhones))
+amountOfPhones = buyPhones(799,199.99)
+print("\nExpected : 799,199.99 → 3 \nYour code: 799,199.99 → " + str(amountOfPhones))
