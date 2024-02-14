1972e321c59b736b6e54b9c7af30983746d6f577
Justin Riley <20hz20khz@gmail.com>
2024-02-13 21:56:45 -0500

    Create ConsoleStringsAndScreams.py

---
+++ _apcsp/console/ConsoleStringsAndScreams/ConsoleStringsAndScreams.py
@@ -0,0 +1,29 @@
+# define a function named getLength which take a name as an argument. Returns the length of the name
+
+# define a function named firstLetter which takes a name as an argument. Returns the first letter of the name
+
+# define a function named lastLetter which takes a name as an argument. Returns the last letter of the name
+
+# define a function named printDetails which takes a name as an argument. Use the getLength, firstLetter and lastLetter functions to print the length, first letter and last letter. Return the name.
+
+# define a function named getNickname with lastname as an argument. Create a variable named nickname and set it to the first 2 letters of lastname plus the first 2 letters of lastname. Print nickname (For example, Your nickname is JoJo). Return nickname.
+
+# define a function named scream with 2 arguments: firstname and lastname. Create a variable named myScream that contains empty string. Make a loop that repeats for the length of the first name + the length of the second name, concatenating an "A" to myScream each repetition. After the loop, concatenate an "!" to myScream. Print myScream. Return myScream. 
+
+def main():
+    print("Enter your first name:")
+    # Collect input
+    # Respond with a welcome message for the user. For example, Hello Steve!
+    
+    # Call the printDetails function with the user's first name as an argument.
+    
+    print("Enter your last name:")
+    # Collect input
+    
+    # Call the printDetails function with the user's last name as an argument.
+    
+    # Print the full name (For example, Your full name is Steve Jobs).
+    
+    # Call the getNickname function with the user's LAST name as an argument.
+    
+    # Finally, scream! Call the scream function which will print a scream the length of the first name plus length of the last name
