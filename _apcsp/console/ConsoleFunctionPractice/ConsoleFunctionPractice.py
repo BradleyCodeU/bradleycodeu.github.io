28a0cf86a9822287924c837f8198955991886f31
Justin Riley <20hz20khz@gmail.com>
2024-02-14 14:31:39 -0500

    Create ConsoleFunctionPractice.py

---
+++ _apcsp/console/ConsoleFunctionPractice/ConsoleFunctionPractice.py
@@ -0,0 +1,38 @@
+
+# Function Practice
+# Part 1
+# startsWithA returns true if str begins with an "A" or begins with an "a". Otherwise it returns false.
+# Examples...
+# startsWithA("Apple") -> True
+# startsWithA("alaska") -> True
+# startsWithA("Ohio") -> False
+
+def startsWithA(str):
+  # your code goes here
+  return
+
+
+
+# Part 2
+# firstAndLast returns the first and last letters of str. Assume that str is always a length of 2 or more.
+# Examples...
+# firstAndLast("Apple") -> "Ae"
+# firstAndLast("alaska") -> "aa"
+# firstAndLast("Ohio") -> "Oo"
+
+def firstAndLast(str):
+  # your code goes here
+  return
+
+
+
+# Part 3
+# middleLetter returns the letter at the middle of str. You will divide the length of str by two, then use the int() function to convert from a decimal to a whole number. Assume that str is always a length of 2 or more.
+# Examples...
+# middleLetter("Apple") -> "p"
+# middleLetter("alaska") -> "s"
+# middleLetter("Ohio") -> "i"
+
+def middleLetter(str):
+  # your code goes here
+  return
