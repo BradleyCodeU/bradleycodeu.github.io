d70943f1a9ecde93cc8b2982e929b5b65d33b50b
Justin Riley <20hz20khz@gmail.com>
2024-02-14 10:10:51 -0500

    Update Tests_PyHogwartsHouse.py

--- _apcsp/py/PyHogwartsHouse/Tests_PyHogwartsHouse.py
+++ _apcsp/py/PyHogwartsHouse/Tests_PyHogwartsHouse.py
@@ -1,9 +1,7 @@
+from PyHogwartsHouse import *
 import unittest
 
-from PyHogwartsHouse import *
-
-
-class Tests_PyHogwartsHouse(unittest.TestCase):
+class AutomaticTester(unittest.TestCase):
 
     def test_validateNumber(self):
         self.assertEquals(validateNumber("1"), True)
@@ -31,34 +29,53 @@ class Tests_PyHogwartsHouse(unittest.TestCase):
         self.assertEquals(getHouse("10","1"), "Slytherin")
 
 
-
 class MyTestResult(unittest.TextTestResult):
-
     def __init__(self, stream, descriptions, verbosity):
         super().__init__(stream, descriptions, verbosity)
         self.stream = stream
         self.verbosity = verbosity
+        self.success_count = 0
+        self.failure_count = 0
 
     def addSuccess(self, test):
+        self.success_count += 1
         if self.verbosity > 0:
-            self.stream.write("✅")
+            self.stream.write("✅  ")
         else:
             super().addSuccess(test)
+        self.stream.writeln(test._testMethodName)
 
     def addFailure(self, test, err):
         super().addFailure(test, err)
-        if self.verbosity > 0:
-            self.stream.write("🛑")
+        self.failure_count += 1
+        self.stream.write("❌")
+        self.stream.writeln(f" {test._testMethodName}")
+
+    def startTestRun(self):
+        super().startTestRun()
+        self.success_count = 0
+        self.failure_count = 0
+
+    def stopTestRun(self):
+        if self.failure_count > 0:
+            self.stream.writeln(
+                f"❌  {self.success_count}/{self.success_count + self.failure_count} passed, see errors below"
+            )
         else:
-            self.stream.writeln(f"FAIL: {test._testMethodName}")
-            self.stream.writeln(err)
+            self.stream.writeln(
+                f"✅  {self.success_count}/{self.success_count + self.failure_count} passed!"
+            )
 
-    def startTest(self, test):
-        unittest.TestResult.startTest(self, test)
-        if self.verbosity > 1:
-            self.stream.write(f"{test._testMethodName}")
-            self.stream.flush()
 
-if __name__ == '__main__':
-    unittest.main(testRunner=unittest.TextTestRunner(resultclass=MyTestResult, verbosity=1),exit=False)
-    #unittest.main(verbosity=1,exit=False)
+if __name__ == "__main__":
+    result = unittest.TextTestRunner(resultclass=MyTestResult, verbosity=1).run(
+        unittest.TestLoader().loadTestsFromTestCase(AutomaticTester)
+    )
+    if result.failures:
+        failed_test_names = [test._testMethodName for test, _ in result.failures]
+        failed_test_names = [name for name in failed_test_names]
+        for each in failed_test_names:
+            print(f"❌  {each}")
+        print()
+    else:
+        print("✅  All tests passed!\n")
