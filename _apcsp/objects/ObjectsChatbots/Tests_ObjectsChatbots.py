04f3c36ab4b2a9c5a05a9fa3857227ea11d5b4ac
Justin Riley <20hz20khz@gmail.com>
2024-02-19 16:54:48 -0500

    chatbots

---
+++ _apcsp/objects/ObjectsChatbots/Tests_ObjectsChatbots.py
@@ -0,0 +1,80 @@
+from ObjectsCastlesAndCreatures import *
+import unittest
+
+
+
+class AutomaticTester(unittest.TestCase):
+    def test_valid_integer(self):
+        self.assertTrue(validNumber("10"))
+        self.assertTrue(validNumber("100"))
+    
+    def test_invalid_integer(self):
+        self.assertFalse(validNumber("-5"))
+        self.assertFalse(validNumber("0"))
+        self.assertFalse(validNumber("abc"))
+        self.assertFalse(validNumber("3.14"))
+
+    def test_getChatbotList(self):
+        self.assertTrue(len(getChatbotList()) >= 2)
+        for eachChatbot of getChatbotList():
+            self.assertTrue(len(eachChatbot) >= 8,"Each chatbot must have 8 key/value pairs.")
+            uniquePhrases = []
+            for key, val in eachChatbot.items():
+                if val not in uniquePhrases:
+                    uniquePhrases.append(val)
+            self.assertTrue(len(uniquePhrases) >= 8,"Each chatbot must have 8 *unique* responses")
+
+
+
+
+
+class MyTestResult(unittest.TextTestResult):
+    def __init__(self, stream, descriptions, verbosity):
+        super().__init__(stream, descriptions, verbosity)
+        self.stream = stream
+        self.verbosity = verbosity
+        self.success_count = 0
+        self.failure_count = 0
+
+    def addSuccess(self, test):
+        self.success_count += 1
+        if self.verbosity > 0:
+            self.stream.write("✅  ")
+        else:
+            super().addSuccess(test)
+        self.stream.writeln(test._testMethodName)
+
+    def addFailure(self, test, err):
+        super().addFailure(test, err)
+        self.failure_count += 1
+        self.stream.write("❌")
+        self.stream.writeln(f" {test._testMethodName}")
+
+    def startTestRun(self):
+        super().startTestRun()
+        self.success_count = 0
+        self.failure_count = 0
+
+    def stopTestRun(self):
+        if self.failure_count > 0:
+            self.stream.writeln(
+                f"❌  {self.success_count}/{self.success_count + self.failure_count} passed, see errors below"
+            )
+        else:
+            self.stream.writeln(
+                f"✅  {self.success_count}/{self.success_count + self.failure_count} passed!"
+            )
+
+
+if __name__ == "__main__":
+    result = unittest.TextTestRunner(resultclass=MyTestResult, verbosity=1).run(
+        unittest.TestLoader().loadTestsFromTestCase(AutomaticTester)
+    )
+    if result.failures:
+        failed_test_names = [test._testMethodName for test, _ in result.failures]
+        failed_test_names = [name for name in failed_test_names]
+        for each in failed_test_names:
+            print(f"❌  {each}")
+        print()
+    else:
+        print("✅  All tests passed!\n")
--- _apcsp/objects/ObjectsChatbots/instructions.md
+++ _apcsp/objects/ObjectsChatbots/instructions.md
@@ -1,9 +1,3 @@
----
-layout: project
-category: objects
-title: Chatbots
-permalink: /apcsp/objects/chatbots/
----
 # Chatbots
 
 Read this w3schools tutorial about [Python dictionaries](https://www.w3schools.com/python/python_dictionaries.asp). Notice that dictionary items are presented in key/value pairs. Here is also a [realpython.com tutorial](https://realpython.com/python-dicts/)
@@ -18,6 +12,8 @@ For example, you might add the keyword "game" by creating the follow key/value p
 The starter template has been provided. Type `talkTo(dan)` in the console to see the demo.
 
 
+
+
 REQUIREMENTS:
   - Create 2 dictionary-based chatbots (if you are working with a partner then create 4 chatbots). Each chatbot must have 8 key/value pairs. Each chatbot must have 8 *unique* responses. 
   - Create a validNumber() function that take one argument: userString. It will return true if the userString is an integer number that is greater than zero. Otherwise false. Use try-except so that it doesn't crash.
@@ -28,57 +24,3 @@ REQUIREMENTS:
     - Based on the player input, call the talkTo() function and pass the variable name of the dictionary as an argument. For example, talkTo(amandaBot) or maybe talkTo(myArray[userinput])
 
 
-
-Type talkTo(dan) in the console to see the demo.
-
-## STARTER CODE
-```
-import time
-import sys
-
-# Type talkTo(dan) in the console to see the demo.
-
-dan = {
-  'name': 'Cowboy Dan',
-  'want': 'I wanna find my missin\' hat',
-  'trade': 'I\'ll trade ya this here pair of boots',
-  'hello': 'Howdy there',
-  'bye': 'See ya later',
-  'default': 'Hot enough for ya?',
-  # Here are 6 example key/value pairs
-  # Your chatbots must each have 8 key/value pairs
-}
-
-def typewriter(text):
-    for char in text:
-        time.sleep(0.05)
-        sys.stdout.write(char)
-        sys.stdout.flush()
-
-def talkSearch(someChatbot,userInput):
-    found = False
-    output = '\n   \"'
-    if 'name' in someChatbot:
-      output = '\n   ' + someChatbot['name'] + ': \"'
-    for key in someChatbot:
-        if key in userInput.lower():
-            if found:
-                output += ', '
-            output += someChatbot[key]
-            found = True
-    if not found and 'default' in someChatbot:
-        output += someChatbot['default']
-    return output + '\"\n'
-
-def talkTo(someChatbot):
-    output = talkSearch(someChatbot, "hello")
-    while True:
-        typewriter(output)
-        print('\nType a question or bye:')
-        userInput = input('>>> ')
-        output = talkSearch(someChatbot, userInput)
-        if 'bye' in userInput.lower():
-            typewriter(output)
-            break
-
-```
