9a4d3af5a8197e368aaa120c64c5ff5b8eb1e840
Justin Riley <20hz20khz@gmail.com>
2024-02-28 19:03:06 -0500

    setup junit

---
+++ _apcsa/ch1introduction/U1ASCIIArt/U1ASCIIArt.java
@@ -0,0 +1,3 @@
+public class U1ASCIIArt {
+    
+}
--- _apcsa/ch1introduction/U1CoinConverter/src/Test_U1CoinConverter.java
+++ _apcsa/ch1introduction/U1CoinConverter/src/Test_U1CoinConverter.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 public class Test_U1CoinConverter {
 
     @Test
-    public void testConvertCoins() {
+    public void testConvertCoins244() {
         assertEquals("2 dollars, 1 quarters, 1 dimes, 1 nickels, 4 pennies", U1CoinConverter.convertCoins(2.44));
+    }
+    @Test
+    public void testConvertCoins555() {
         assertEquals("5 dollars, 2 quarters, 0 dimes, 1 nickels, 0 pennies", U1CoinConverter.convertCoins(5.55));
+    }
+    @Test
+    public void testConvertCoins999() {
         assertEquals("9 dollars, 3 quarters, 2 dimes, 0 nickels, 4 pennies", U1CoinConverter.convertCoins(9.99));
     }
 }
