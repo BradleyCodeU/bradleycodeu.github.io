20dbbce7842b0fe646cf7106cd23a2896158dc68
Justin Riley <20hz20khz@gmail.com>
2024-03-18 23:34:29 -0400

    Update PriceTracker.java

--- _apcsa/u06b_fe/U06bFarmersMarket/src/PriceTracker.java
+++ _apcsa/u06b_fe/U06bFarmersMarket/src/PriceTracker.java
@@ -3,6 +3,7 @@ public class PriceTracker {
     private int demand;
     private double price;
     private double previousPrice;
+    private double idealMarketPrice; // the price businesses want
     private double minimumPrice;
 
     public PriceTracker(int supply, int demand, double price) {
@@ -10,7 +11,8 @@ public class PriceTracker {
         this.demand = demand;
         this.price = price;
         this.previousPrice = price;
-        this.minimumPrice = 1 + Math.random() + Math.random();
+        this.idealMarketPrice = Math.random() + Math.random();
+        this.minimumPrice = this.idealMarketPrice * 0.5;
         // allow the price to reach a more normal level by updating 100 times
         for(int i=0;i<100;i++){
             update();
@@ -22,15 +24,15 @@ public class PriceTracker {
         // Adjust demand based on price
         if (price > previousPrice) {
             demand -= (int) (Math.random() * 6) + (int) (Math.random() * 6);  // Decrease demand if price is above average price
-        } else if (price < previousPrice) {
+        } else if (price <= previousPrice) {
             demand += (int) (Math.random() * 6) + (int) (Math.random() * 6);  // Increase demand if price is below average price
         }
 
-        // Adjust supply based on price
-        if (price > previousPrice) {
-            supply -= (int) (Math.random() * 6) + (int) (Math.random() * 6);  // Decrease supply if price exceeds average price
-        } else if (price < previousPrice) {
-            supply += (int) (Math.random() * 6) + (int) (Math.random() * 6);  // Increase supply if price is less than average price
+        // Adjust supply based on ideal market price (the price businesses want)
+        if (price < idealMarketPrice) {
+            supply -= (int) (Math.random() * 6);  // Decrease supply if price exceeds average price
+        } else if (price > idealMarketPrice) {
+            supply += (int) (Math.random() * 6) + (int) (Math.random() * 6) + (int) (Math.random() * 6);  // Increase supply if price is less than average price
         }
 
         previousPrice = price;
@@ -39,12 +41,12 @@ public class PriceTracker {
         if (supply > demand && price > minimumPrice) {
             price *= (0.99 - Math.abs(price) * 0.01); // Decrease price if supply exceeds demand
         } else if (supply < demand) {
-            price *= 1.01;  // Increase price if demand exceeds supply
+            price *= 1.05;  // Increase price if demand exceeds supply
         }
 
-        // Reduce the supply by 1/10 demand
-        if (supply > demand / 10) {
-            supply -= demand / 10;
+        // Reduce the supply by 1/2 demand
+        if (supply > demand / 2) {
+            supply -= demand / 2;
         } else {
             supply = 0;
             demand = (int) (0.9 * demand);
