2ca09d690323adc1f04d7193123a709d27292e86
Justin Riley <20hz20khz@gmail.com>
2024-03-25 10:53:01 -0400

    Create App.java

---
+++ _apcsa/u05b_sv/Grading_ParabolaFRQ/src/App.java
@@ -0,0 +1,106 @@
+class Parabola{
+
+
+    private int a;
+    
+    
+    private int b;
+    
+    
+    private int c;
+    
+    
+     
+    
+    
+     
+    
+    
+    public Parabola(int a_, int b_, int c_){
+    
+    
+    a=a_;
+    
+    
+    b=b_;
+    
+    
+    c=c_;
+    
+    
+    }
+    
+    
+    public double getAxis(){
+    
+    
+    return ( ((double) b)/(a*-2) );
+    
+    
+    }
+    
+    
+    public boolean  isOnGraph(int x, int y){
+    
+    
+     
+    
+    
+    if(( (a*x*x)+(b*x)+(c) )== y ){
+    
+    
+    return true;
+    
+    
+    }
+    
+    
+    else{
+    
+    
+    return false;
+    
+    
+    }
+    
+    
+     
+    
+    
+    }
+    
+    
+    }
+
+
+public class App {
+    public static void main(String[] args) {
+        // First Parabola
+        Parabola par1 = new Parabola(2, -6, -5);
+        double expectedAxis1 = 1.5;
+        boolean expectedOnGraph1 = true;
+
+        double actualAxis1 = par1.getAxis();
+        boolean actualOnGraph1 = par1.isOnGraph(4, 3);
+
+        System.out.println("Expected Axis for Parabola 1: " + expectedAxis1);
+        System.out.println("Actual Axis for Parabola 1: " + actualAxis1);
+        System.out.println("Expected isOnGraph for Parabola 1: " + expectedOnGraph1);
+        System.out.println("Actual isOnGraph for Parabola 1: " + actualOnGraph1);
+
+        // Second Parabola
+        Parabola par2 = new Parabola(4, 2, -2);
+        double expectedAxis2 = -0.25;
+        boolean expectedOnGraph2 = false;
+
+        double actualAxis2 = par2.getAxis();
+        boolean actualOnGraph2 = par2.isOnGraph(4, 3);
+
+        System.out.println("\n\nExpected Axis for Parabola 2: " + expectedAxis2);
+        System.out.println("Actual Axis for Parabola 2: " + actualAxis2);
+        System.out.println("Expected isOnGraph for Parabola 2: " + expectedOnGraph2);
+        System.out.println("Actual isOnGraph for Parabola 2: " + actualOnGraph2);
+    
+        System.out.println("\n\nENSURE THAT INSTANCE VARS ARE PRIVATE ! ! !\ngetAxis should be (-b/2a)\nisOnGraph should be y == ax2 + bx + c");
+    }
+}
