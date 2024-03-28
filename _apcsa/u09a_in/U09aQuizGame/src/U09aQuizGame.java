1e18c332d0415a48c59a1b3f8cba5e23d753aee9
Justin Riley <20hz20khz@gmail.com>
2024-03-28 07:35:38 -0400

    Update U09aQuizGame.java

--- _apcsa/u09a_in/U09aQuizGame/src/U09aQuizGame.java
+++ _apcsa/u09a_in/U09aQuizGame/src/U09aQuizGame.java
@@ -9,7 +9,7 @@ public class U09aQuizGame {
                 new TrueFalseQuestion("3 * 3 * 3 = 9", false, 10),
                 new TrueFalseQuestion("4! = 4 * 3 * 2 * 1", true, 10),
                 new MultipleChoiceQuestion("What is 2 * 2?", "4",new String[]{"22","44","4","2"}, 10),
-                new Question("What is 2 + 2?", "4", 10),
+                new Question("What is 2 + 2?", "4"),
                 new Question("Who wrote 'So Long, and Thanks for All the Fish (The Hitchhiker's Guide to the Galaxy, #4)'?", "Adams", 10),
                 new Question("Who wrote 'Romeo and Juliet'?", "Shakespeare", 10)
         };
