70af480b5e84362bf159e6cf76edd1a73d6a3d98
Justin Riley <20hz20khz@gmail.com>
2024-08-19 14:34:07 -0400

    Create U02aDollarApp.java

---
+++ _apcsa/u02a_uo/U02aDollar/src/U02aDollarApp.java
@@ -0,0 +1,36 @@
+public class U02aDollarApp {
+    public static void main(String[] args){
+        // TODO calculate the following currency problems using double
+
+        System.out.println("Q1)   $0.70 + $0.10 = ?");
+
+        // TODO print what you EXPECT the answer to be
+
+        System.out.println("EXPECT ?");
+
+        // TODO print the ACTUAL result using doubles
+
+        System.out.println("ACTUAL ?");
+
+        System.out.println("Q2)   $0.10 + $0.20 = ?");
+
+        // TODO print what you EXPECT the answer to be
+
+        // TODO print the ACTUAL result using doubles
+
+        // Notice that Mr Riley has given you a partial Dollar class.
+        // Use the Dollar class for Questions 3 and 4...
+
+        System.out.println("Q3)   $0.70 + $0.10 = ?");
+
+        // TODO print what you EXPECT the answer to be
+
+        // TODO print the ACTUAL result using Dollar objects and the add() method
+
+        System.out.println("Q4)   $0.10 + $0.20 = ?");
+
+        // TODO print what you EXPECT the answer to be
+
+        // TODO print the ACTUAL result using Dollar objects and the add() method
+    }
+}
