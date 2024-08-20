d01b2e4584e8efeb7ef943faf5d58a0e95a2b081
Justin Riley <20hz20khz@gmail.com>
2024-08-20 19:18:42 -0400

    Update U01aPaintCalcIntOnly.java

--- _apcsa/u01a_pt/U01aPaintCalcIntOnly/src/U01aPaintCalcIntOnly.java
+++ _apcsa/u01a_pt/U01aPaintCalcIntOnly/src/U01aPaintCalcIntOnly.java
@@ -10,9 +10,10 @@ public class U01aPaintCalcIntOnly {
 
   public static int calculatePaintGallons(int total_area)
   {
-    // TODO Calculate the number of gallons of paint needed
+    // TODO Calculate the number of gallons of paint needed PLUS 1
     // Assume that 1 gallon can of paint will cover 400 square feet PLUS always buy 1 extra
     // For example...
+    // calculatePaintGallons(0) --> 1
     // calculatePaintGallons(350) --> 1
     // calculatePaintGallons(432) --> 2
     return 0;
@@ -21,8 +22,8 @@ public class U01aPaintCalcIntOnly {
   public static int calculateCostEstimate(int gallons_needed, int price_per_gallon, int sales_tax_percent)
   {
     // TODO Calculate the total cost including sales tax PLUS 1
-    // Beware of integer division when you try to calculate the sales tax
     // For example...
+    // calculateCostEstimate(1, 16, 10) --> 18
     // calculateCostEstimate(2, 16, 10) --> 36
     // calculateCostEstimate(4, 16, 10) --> 71
 
