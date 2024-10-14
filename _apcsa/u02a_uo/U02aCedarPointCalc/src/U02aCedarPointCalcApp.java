d7208fd6c1b45faae037ae9ffc3180e85b4c4259
Justin Riley <20hz20khz@gmail.com>
2024-10-14 11:50:25 -0400

    add dollar class to cedar point

---
+++ _apcsa/u02a_uo/U02aCedarPointCalc/src/U02aCedarPointCalcApp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class U02aCedarPointCalcApp {
+
+    // Documentation for the Dollar class is here: 
+    // https://bradleycodeu.github.io/apcsa/u02a_uo/U02aCedarPointCalc/doc/Dollar.html
+    // A new subtraction method has been added...
+    // Dollar sub(Dollar other)
+    // A new multiplication method has been added...
+    // Dollar mult(double multiplier)
+
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        // TODO see instructions.md 
+
+
+
+        System.out.println("=====RESULTS=====");
+
+        // TODO see instructions.md 
+
+
+
+    }
+}
