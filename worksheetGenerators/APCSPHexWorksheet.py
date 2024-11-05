34fa7eb43de297166c371a35a9662efea86a2996
Justin Riley <20hz20khz@gmail.com>
2024-11-05 15:05:13 -0500

    updated worksheets

--- worksheetGenerators/APCSPHexWorksheet.py
+++ worksheetGenerators/APCSPHexWorksheet.py
@@ -2,58 +2,52 @@ import random
 
 b10="\u208d\u2081\u2080\u208e"
 b16="\u208d\u2081\u2086\u208e"
-mylist=["00FC",252,225,"00E1"]
+mylist=["00FC",252,225,"00E1",0,1,16,256,4096]
 print(
 """
 Take a screenshot and complete in Notability OR copy/paste
 the output into a text editor like Google Docs, Microsoft
 Word, or the iOS Notes app. Set the font to COURIER NEW
-and complete the worksheet.
-"""
-)
-print("""                          _           _                 _
+and complete the worksheet_           _                 _ 
   /\  /\_____  ____ _  __| | ___  ___(_)_ __ ___   __ _| |
  / /_/ / _ \ \/ / _` |/ _` |/ _ \/ __| | '_ ` _ \ / _` | |
 / __  /  __/>  < (_| | (_| |  __/ (__| | | | | | | (_| | |
 \/ /_/ \___/_/\_\__,_|\__,_|\___|\___|_|_| |_| |_|\__,_|_|
-Hexadecimal Worksheet
+Hexadecimal Worksheet                   Name: ______________________________
 Hex is a base-16 system that uses 0123456789ABCDEF
 A=10 B=11 C=12 D=13 E=14 F=15
 Columns are: 4096s 256s 16s 1s
 """)
 
-for i in range(1):
-    print("                        Name: _______________")
-
-print("\nPart A: Convert the base-16 number to decimal")
+print("Part A: Convert the base-16 number to decimal")
 print("  For example, 00FC"+b16+" = 252"+b10)
 for i in range(6):
-    temp = str(hex(random.randint(i*25,i*50+2)+random.randint(i*25,i*50+2)+10))
-    temp = temp[2:].zfill(4).upper()
+    temp = random.randint(i*50,i*50+50)+random.randint(i*50,i*50+50)+17
+    # temp = temp[2:].zfill(4).upper()
     while temp in mylist:
-      temp = str(hex(random.randint(i*25,i*50+2)+random.randint(i*25,i*50+2)+10))
-      temp = temp[2:].zfill(4).upper()
+      temp = random.randint(i*50,i*50+50)+random.randint(i*50,i*50+50)+17
+      # temp = temp
     mylist.append(temp)
     print(
         "%6s.) %22s"
         % (
             str(i + 1),
-            str(temp) + b16 + " = ______"+b10,
+            str(str(hex(temp))[2:].zfill(4).upper()) + b16 + " = ______"+b10,
         )
     )
 print("\nPart B: Convert the decimal number to base-16")
 print("  For example, 225"+ b10 +" = 00E1"+b16)
-for i in range(6):
-    temp = random.randint(i*5+1, i * 15 + 10) + random.randint(i*5, i * 15 + 10)+10
+for i in range(7,13):
+    temp = random.randint(i*11, i * 11 + 10) + random.randint(i*11, i * 11 + 10)+17
     if temp%2==0:temp+=1
     while temp in mylist:
-      temp = random.randint(i*5+1, i * 15 + 10) + random.randint(i*5, i * 15 + 10)+10
+      temp = random.randint(i*11, i * 11 + 10) + random.randint(i*11, i * 11 + 10)+17
       if temp%2==0:temp+=1
     mylist.append(temp)
     print(
         "\n%6s.) %25s"
         % (
-            str(i + 7),
+            str(i),
             str(temp) + b10 + " = __________"+b16,
         )
     )
--- worksheetGenerators/APCSPOctalWorksheet.py
+++ worksheetGenerators/APCSPOctalWorksheet.py
@@ -1,53 +1,50 @@
 import random
 
-mylist=["0321",221]
+mylist=[0,1,8,64,209,221,512]
 print(
 """
 Take a screenshot and complete in Notability OR copy/paste
 the output into a text editor like Google Docs, Microsoft
 Word, or the iOS Notes app. Set the font to COURIER NEW
-and complete the worksheet.
-"""
+and complete the worksheet."""
 )
-print("""\n  __    ___  ____   __    __
- /  \  / __)(_  _) /  \  (  )
+print("""  __    ___  ____   __    __   
+ /  \  / __)(_  _) /  \  (  )  
 (  O )( (__   )(  /  O \ / (_/\\
  \__/  \___) (__) \_/\_/ \____/""")
-print("Octal Worksheet")
+print("Octal Worksheet             Name: _______________")
 print("Hint: 512s 64s 8s 1s")
-for i in range(1):
-    print("                     Name: _______________")
 
 print("\nPart A: Convert the octal number to decimal")
 print("For example, 0321\u208d\u2088\u208e = 209\u208d\u2081\u2080\u208e")
-for i in range(6):
+for i in range(1,7):
     temp = (
-        str(random.randint(0, int(i / 3)))
-        + str(random.randint(0, int(i / 2) + 1))
-        + str(random.randint(0, i + 1))
-        + str(random.randint(i + 1, 7))
+        str(int(i / 4))
+        + str(random.randint(int(i / 2), i))
+        + str(i + 1)
+        + str(random.randint(i, 7))
     )
-    while temp in mylist:
+    while int(temp) in mylist:
       temp = (
-        str(random.randint(0, int(i / 3)))
-        + str(random.randint(0, int(i / 2) + 1))
-        + str(random.randint(0, i + 1))
-        + str(random.randint(i + 1, 7))
+        str(int(i / 4))
+        + str(random.randint(int(i / 2), i))
+        + str(i + 1)
+        + str(random.randint(i, 7))
       )
-    mylist.append(temp)
+    mylist.append(int(temp))
     print(
         "%6s.) %21s"
         % (
-            str(i + 1),
+            str(i),
             str(temp) + "\u208d\u2088\u208e = ______\u208d\u2081\u2080\u208e",
         )
     )
 print("\nPart B: Convert the decimal number to octal")
 print("For example, 221\u208d\u2081\u2080\u208e = 0335\u208d\u2088\u208e")
 for i in range(6):
-    temp = random.randint(i*10+1, i * 25 + 10) + random.randint(i*10, i * 25 + 10)
+    temp = random.randint(i*52 + 9, i * 52 + 29) + random.randint(i*52 + 9, i * 52 + 29)
     while temp in mylist:
-      temp = random.randint(i*10+1, i * 25 + 10) + random.randint(i*10, i * 25 + 10)
+      temp = random.randint(i*52 + 9, i * 52 + 29) + random.randint(i*52 + 9, i * 52 + 29)
     mylist.append(temp)
     print(
         "\n%6s.) %29s"
