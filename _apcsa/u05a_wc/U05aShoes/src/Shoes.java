41722a910dd8d2a24e8ed55ddaf846323e479100
Justin Riley <20hz20khz@gmail.com>
2025-01-09 12:05:01 -0500

    Update Shoes.java

--- _apcsa/u05a_wc/U05aShoes/src/Shoes.java
+++ _apcsa/u05a_wc/U05aShoes/src/Shoes.java
@@ -1,3 +1,26 @@
 public class Shoes {
-    
-}
+    // instance variables go here
+    // create private instance vars for brand, type, and size
+    private String color;
+  
+    // methods go here
+    // create public methods setBrand, setType, setSize, getModelNumber
+    public void setColor(String _color){
+      color = _color;
+    }
+  
+    // setBrand
+  
+    // setType
+  
+    // setSize
+  
+    // getModelNumber
+  
+    // update the toString method to return expected output
+    // NOTE: do NOT include the model number here!
+    public String toString(){
+      return "Shoes that are color=" + color;
+    }
+  
+  }
