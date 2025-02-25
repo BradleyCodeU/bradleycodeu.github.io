7f571cb8b85228a875d65d92bb70a52e9238e2d1
Justin Riley <20hz20khz@gmail.com>
2025-02-25 10:52:47 -0500

    Update FarmersMarket.java

--- _apcsa/u06b_fe/U06bFarmersMarket/src/FarmersMarket.java
+++ _apcsa/u06b_fe/U06bFarmersMarket/src/FarmersMarket.java
@@ -20,26 +20,30 @@ public class FarmersMarket {
 
         // increment the cityCounter
 
-        // Initialize PriceTrackers for corn (index 0 in PriceTracker array), apples (index 1), eggs (index 2)
-        // Use a loop to construct all of the trackers in the trackerArray. use the no-args constructor PriceTracker()
+        // Create a PriceTracker array of size 3 for corn (index 0 in PriceTracker array), apples (index 1), eggs (index 2)
+        // Use a for-loop to Construct all of the trackers in the trackerArray. use the no-args constructor PriceTracker()
 
     }
 
 
+
     // updateAllTrackers() Method to update prices of all items by using the update() method on each item's PriceTracker.
-    // Use a loop to update all of the trackers in the trackerArray.
+    // Use a for each loop to update all of the trackers in the trackerArray.
     // No parameters. Returns nothing.
+
 
 
     // getName Method to get the String name
     // Returns: the name of this FarmersMarket
 
 
-    // getPriceTracker(String name) Method to get the PriceTracker object for a specific item
+
+    // getPriceTracker(String name) Method to get the PriceTracker object for a specific item. corn is index 0, apples 1, eggs 2.
     // Parameters:
     // - String item: The name of the item. Either "corn", "apples", or "eggs"
     // Returns:
     // - The PriceTracker object for the specified item or null if the String is invalid
+
 
 
     // getAvailableSupply(String name) Method to get the available supply of a specific item
@@ -49,10 +53,12 @@ public class FarmersMarket {
     // - The available supply of the specified item or 0 if the String is invalid
 
 
+
     // toString() Method to generate a string representation of the FarmersMarket object
     // Returns:
     // - A string containing the name of the market and the prices and supplies of all items 
     //   with "\n" separating each item. Use the PriceTracker's toString to easily get the price and supply.
+
 
 
 }
