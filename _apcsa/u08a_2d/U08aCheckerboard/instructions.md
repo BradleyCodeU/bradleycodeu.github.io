85ef82e32db8fd810d8d68bdc4b5dbab2de8d7b6
Justin Riley <20hz20khz@gmail.com>
2025-03-26 11:02:11 -0400

    checkerboard

--- _apcsa/u08a_2d/U08aCheckerboard/instructions.md
+++ _apcsa/u08a_2d/U08aCheckerboard/instructions.md
@@ -7,12 +7,17 @@ Write a program that creates a checkerboard and allows the user to place numbers
 In addition to creating a main method. You must create the following static methods:
   - public static boolean isValid(int userInput, int size) // returns true if userInput is in the range of [0...size) exclusive of size
   - public static void displayCheckerboard(int[][] arr) // prints the checkboard
-  - public static int[][] fill2DArray(int size) // Returns a 2d array fill with alternating 0s and 1s. USING TWO NESTED LOOPS, initialize the board so that zeros and ones alternate, as on a checkerboard. <small>Hint: Check whether i + j is even.</small>
+  - public static int[][] fill2DArray(int rows, int cols) // Returns a 2d array fill with alternating 0s and 1s. 
+    - //         USING TWO NESTED LOOPS, initialize the board so that zeros and ones alternate, as on a checkerboard. <small>Hint: Check whether i + j is even.</small>
+    - //         For example...
+    - //          0  1  0
+    - //          1  0  1
+    - //          0  1  0
 
 Directions for the main method:
  - prompt the user for a size (an integer > 1)
  - declare the 2d array and set it to be fill2DArray(size)
- - loop size^2 times
+ - loop width * height times
  - - print the checkerboard using the displayCheckerboard() method
  - - prompt the user for the value that they want to place
  - - prompt the user for the row number
@@ -25,7 +30,8 @@ Directions for the main method:
 
 Sample Run 1:
 ```
-Gimme an integer >1: 3
+Gimme a width >1: 3
+Gimme a height >1: 3
 0  1  0
 1  0  1
 0  1  0
@@ -39,11 +45,12 @@ Gimme a col: 1
 
 Sample Run 2:
 ```
-Gimme an integer >1: 4
+Gimme a width >1: 3
+Gimme a height >1: -1
+Oops! Gimme a height >1: 2
 0  1  0  1  
 1  0  1  0
 0  1  0  1
-1  0  1  0  
 Gimme a value: 9
 Gimme a row: 2
 Gimme a col: -1
@@ -51,12 +58,10 @@ Invalid
 0  1  0  1  
 1  0  1  0
 0  1  0  1
-1  0  1  0  
 Gimme a value: 9
 Gimme a row: 2
 Gimme a col: 2
 0  1  0  1  
 1  0  1  0
 0  1  9  1
-1  0  1  0
 ```
--- _apcsa/u08a_2d/U08aCheckerboard/src/U08aCheckerboardApp.java
+++ _apcsa/u08a_2d/U08aCheckerboard/src/U08aCheckerboardApp.java
@@ -5,28 +5,42 @@ public class U08aCheckerboardApp {
 
 
     // TODO: - public static boolean isValid(int userInput, int size) // returns true if userInput is in the range of [0...size) exclusive of size
-    // TODO: - public static void displayCheckerboard(int[][] arr) // prints the checkboard
-    // TODO: - public static int[][] fill2DArray(int size) // Returns a 2d array fill with alternating 0s and 1s. 
-    //         USING TWO NESTED LOOPS, initialize the board so that zeros and ones alternate, as on a checkerboard. <small>Hint: Check whether i + j is even.</small>
 
+
+
+    // TODO: - public static void displayCheckerboard(int[][] arr) // prints the checkboard
+
+
+    
+    // TODO: - public static int[][] fill2DArray(int rows, int cols) // Returns a 2d array fill with alternating 0s and 1s. 
+    //         USING TWO NESTED LOOPS, initialize the board so that zeros and ones alternate, as on a checkerboard. <small>Hint: Check whether i + j is even.</small>
+    //         For example...
+    //          0  1  0
+    //          1  0  1
+    //          0  1  0
 
 
 
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Gimme an integer >1: "); // prompt the user for a size (an integer > 1)
-        int size = input.nextInt();
-        
-        while (size <= 1) {
-            System.out.print("Oops! Gimme an integer >1: ");
-            size = input.nextInt();
+        System.out.print("Gimme a width >1: "); // prompt the user for a size (an integer > 1)
+        int width = input.nextInt();
+        while (width <= 1) {
+            System.out.print("Oops! Gimme a width >1: ");
+            width = input.nextInt();
+        }
+        System.out.print("Gimme a height >1: "); // prompt the user for a size (an integer > 1)
+        int height = input.nextInt();
+        while (height <= 1) {
+            System.out.print("Oops! Gimme a height >1: ");
+            height = input.nextInt();
         }
         
         // TODO: declare the 2d array and set it to be fill2DArray(size)
 
         
-        for (int i = 0; i < size * size; i++) {
+        for (int i = 0; i < width * height; i++) {
             // TODO: print the checkerboard using the displayCheckerboard() method
             
             System.out.print("Gimme a value: "); // prompt the user for the value that they want to place
