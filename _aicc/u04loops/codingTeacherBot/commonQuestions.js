ab819653a385270555f55152099afcedd2519695
ohiofi <20hz20khz@gmail.com>
2025-10-21 13:45:24 -0400

    Create commonQuestions.js

---
+++ _aicc/u04loops/codingTeacherBot/commonQuestions.js
@@ -0,0 +1,99 @@
+[
+    [
+        "I am getting an error message",
+        "I am confused",
+        "I'm not sure what to do",
+        "I am trying to get this to work",
+    ], 
+    [
+        "My code breaks when I add this line",
+        "My app breaks when I click the button",
+        "The game breaks when I get to level 2",
+    ], 
+    [
+        "I can't get it to work",
+        "I can't find the mistake",
+        "I can't figure it out",
+    ], 
+    [
+        "My app crashes when I click the button",
+        "My game crashes when I try to log in",
+        "My code crashes when I press start",
+        "It crashes when I open this page",
+    ], 
+    [
+        "My program doesn't run",
+        "My code doesn't work when I use this variable",
+        "The sprite doesn't jump when you press spacebar",
+    ], 
+    [
+        "I don't understand this error message ",
+        "I don't know what's wrong",
+        "I don't get why it doesn't work",
+    ], 
+    [
+        "My program freezes when I run the loop",
+        "My code freezes when I do this",
+        "The app freezes after a few seconds",
+    ], 
+    [
+        "My program gives an error when I call the function",
+        "It gives me an error when I do this",
+        "The editor gives me a weird warning",
+    ], 
+    [
+        "My code is broken",
+        "There is an error on line 10",
+        "Something is wrong ",
+        "It's giving me is a syntax error",
+        "It is printing the wrong thing",
+        "It's stuck in a loop",
+        "It's frozen",
+        "It's running when it's not supposed to",
+        "It's not doing anything",
+        "My code isn't working",
+        "My partner isn't doing anything on the project",
+    ], 
+    [
+        "My app keeps crashing",
+        "It keeps crashing",
+        "It keeps giving me an error",
+    ], 
+    [
+        "It says I have a syntax error",
+        "The error says there is problem on line 42",
+        "My editor says there's something wrong on line 42",
+    ], 
+    [
+        "My program stops working when I enter text",
+        "It stops running when you click this button",
+        "The code stops responding after a few seconds",
+    ], 
+    [
+        "I want it to do something when I click this button",
+        "I want it to keep running when you press OK",
+        "I want the game to keep going",
+    ], 
+    [
+        "It won't compile",
+        "It won't load",
+        "My app won't start",
+        "My code won't run",
+    ]
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
