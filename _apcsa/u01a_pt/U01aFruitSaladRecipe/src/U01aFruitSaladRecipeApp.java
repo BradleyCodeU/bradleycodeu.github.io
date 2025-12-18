0254a1fea4387b829dd778dd3b1628a8203e5130
ohiofi <20hz20khz@gmail.com>
2025-12-18 10:43:30 -0500

    add fruit salad projects

---
+++ _apcsa/u01a_pt/U01aFruitSaladRecipe/src/U01aFruitSaladRecipeApp.java
@@ -0,0 +1,38 @@
+public class U01aFruitSaladRecipeApp {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // # fruit salad recipe
+
+        // print("How many people are you serving?")
+
+        // servingSize = input()
+        // # turn str into int   
+        // servingSize = int(servingSize)
+
+        // # banana
+        // banana = servingSize / 2
+
+        // # apple
+        // apple = servingSize * 0.75
+
+        // # pineapple
+        // pineapple = servingSize * 0.1
+
+        // # cherry
+        // cherry = servingSize * 2.5
+
+        // # watermelon
+        // watermelon = pineapple / 2
+
+        // # grapes
+        // grapes = cherry
+
+        // # convert to string  
+        // banana = str(banana)
+        // cherry = str(cherry)
+
+        // print("You will need " + banana + " bananas")
+        // print("You will need " + cherry + " cherries")
+    }
+}
