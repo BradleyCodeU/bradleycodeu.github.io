61e122c90089cb05bca13d00d60eb8bc9292a694
ohiofi <20hz20khz@gmail.com>
2026-03-13 11:32:25 -0400

    Create StringLoopAlgorithmsApp.java

---
+++ _apcsa/207_iteration/StringLoopAlgorithms/src/StringLoopAlgorithmsApp.java
@@ -0,0 +1,8 @@
+public class StringLoopAlgorithmsApp {
+
+    // Directions for this project are here: https://bradleycodeu.github.io/apcsa/207_iteration/StringLoopAlgorithms/instructions/
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
