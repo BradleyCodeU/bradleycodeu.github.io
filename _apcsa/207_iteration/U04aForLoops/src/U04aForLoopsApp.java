5459141339c259056c001d00172e7f3e7848865a
ohiofi <20hz20khz@gmail.com>
2026-03-13 12:59:31 -0400

    Update U04aForLoopsApp.java

--- _apcsa/207_iteration/U04aForLoops/src/U04aForLoopsApp.java
+++ _apcsa/207_iteration/U04aForLoops/src/U04aForLoopsApp.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class U04aForLoopsApp {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    // See the instructions
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // --- First Input Section ---
+        System.out.print("Gimme a positive integer: ");
+        int a = input.nextInt();
+
+        // Calling the first two methods and the digit method
+        int evenSum = sumEvensExclusive(a);
+        int squareSum = sumSquaresExclusive(a);
+        int digitSum = sumOddDigits(a);
+
+        System.out.println("The sum of all even numbers between 2 and " + a + " (exclusive) = " + evenSum);
+        System.out.println("The sum of all squares between 1 and " + a + " (exclusive) = " + squareSum);
+
+        // --- Second Input Section ---
+        System.out.print("Gimme a positive integer larger than " + a + ": ");
+        int b = input.nextInt();
+
+        // Calling the range method
+        int rangeSum = sumOddsInRangeExclusive(a, b);
+
+        System.out.println("The sum of all odd numbers between " + a + " and " + b + " (exclusive) = " + rangeSum);
+        System.out.println("The sum of all odd digits of " + a + " = " + digitSum);
+        
+        input.close();
     }
 }
