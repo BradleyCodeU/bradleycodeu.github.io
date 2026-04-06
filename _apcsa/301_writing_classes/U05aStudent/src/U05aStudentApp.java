832a6eb2c1eb7d4772740a623235be3278a37aab
ohiofi <20hz20khz@gmail.com>
2026-04-06 10:43:14 -0400

    Create U05aStudentApp.java

---
+++ _apcsa/301_writing_classes/U05aStudent/src/U05aStudentApp.java
@@ -0,0 +1,22 @@
+
+
+public class U05aStudentApp {
+  public static void main(String[] args) {
+    Student demoStudent = new Student("x");
+    System.out.println("Added the following quiz scores: 95, 65, 85");
+    demoStudent.addQuiz(-1); // FIXME
+    demoStudent.addQuiz(-1); // FIXME
+    demoStudent.addQuiz(-2); // FIXME
+    System.out.println("Expected Total = ");
+    System.out.println("Actual Total = ");
+
+    System.out.println("Expected Average = ");
+    System.out.println("Actual Average = ");
+    
+    System.out.println("Name = ");
+    System.out.println("Id Number = ");
+    System.out.println("Email = ");
+    
+    
+  }
+}
