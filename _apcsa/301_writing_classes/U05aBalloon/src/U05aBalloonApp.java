4c16d627d9d02e5ff01e5de7fe4074555833f45b
ohiofi <20hz20khz@gmail.com>
2026-04-06 10:23:37 -0400

    Create U05aBalloonApp.java

---
+++ _apcsa/301_writing_classes/U05aBalloon/src/U05aBalloonApp.java
@@ -0,0 +1,15 @@
+public class U05aBalloonApp {
+  public static void main(String[] args) {
+    System.out.println("Expected volume for radius 5 is 523.6");
+    /* Construct Balloon */
+    /* Inflate Balloon to a radius of 5 */
+    /* getVolume of Balloon */
+    System.out.println("Actual volume: " + balloon1.getVolume());
+    
+    System.out.println("Expected volume for radius 10 is 4188.79");
+    /* Inflate Balloon MORE */
+    /* getVolume of Balloon */
+    System.out.println("Actual volume: " + balloon1.getVolume());
+    
+  }
+}
